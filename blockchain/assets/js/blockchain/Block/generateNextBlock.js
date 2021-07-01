ede7258749569e791479ace3e53d9fc4e011ed6c
MineClash34 <mineclashdu34@gmail.com>
2021-07-01 20:34:59 +0200

    Add promise to return the info(s) relative to the blockchain due to the database add

--- blockchain/assets/js/blockchain/Block/generateNextBlock.js
+++ blockchain/assets/js/blockchain/Block/generateNextBlock.js
@@ -2,10 +2,10 @@ const getLatestBlock = require("./getLatestBlock.js");
 const calculateHash = require("../Hash/calculateHash.js");
 const BlockChain = require("../BlockChain/basicBlockChain.js");
 
-module.exports = (blockData) => {
+module.exports = async (blockData) => {
     var Block = BlockChain.getBlock;
-    var previousBlock = getLatestBlock();
-    var nextIndex = previousBlock.index + 1;
+    var previousBlock = await getLatestBlock();
+    var nextIndex = Number(previousBlock.index) + 1;
     var nextTimestamp = new Date().getTime() / 1000;
     var nextHash = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData);
     return new Block(nextIndex, previousBlock.hash, nextTimestamp, blockData, nextHash);
--- blockchain/assets/js/blockchain/Block/getLatestBlock.js
+++ blockchain/assets/js/blockchain/Block/getLatestBlock.js
@@ -1,6 +1,8 @@
 const BlockChain = require("../BlockChain/basicBlockChain.js");
 
-module.exports = () => {
-    var blockchain = BlockChain.getBlockChain;
-    return blockchain[blockchain.length - 1];
+module.exports = async () => {
+    return new Promise(async function(resolve, reject) {
+        var blockchain = await BlockChain.getBlockChain();
+        resolve(blockchain[blockchain.length - 1]);
+    })
 }
