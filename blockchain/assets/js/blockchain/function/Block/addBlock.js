b6b85292721375faff1baa4efb8647ab85bd9069
MineClash34 <mineclashdu34@gmail.com>
2021-07-01 20:36:14 +0200

    Delete a useless directory (lol, idk why it was here)

--- blockchain/assets/js/blockchain/function/Block/addBlock.js
+++
@@ -1,9 +0,0 @@
-const isValidNewBlock = require("./isValidNewBlock.js");
-const getLatestBlock = require("./getLatestBlock.js");
-const BlockChain = require("../BlockChain/basicBlockChain.js");
-
-module.exports = (newBlock) => {
-    if (isValidNewBlock(newBlock, getLatestBlock())) {
-        BlockChain.BlockChainPush(newBlock);
-    }
-};
--- blockchain/assets/js/blockchain/function/Block/getLatestBlock.js
+++
@@ -1,6 +0,0 @@
-const BlockChain = require("../BlockChain/basicBlockChain.js");
-
-module.exports = () => {
-    var blockchain = BlockChain.getBlockChain;
-    blockchain[blockchain.length - 1];
-}
